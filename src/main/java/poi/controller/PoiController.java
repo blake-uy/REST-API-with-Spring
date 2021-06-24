f97e0b19ed9bde5b718e23b961a39e57a2df374b
blake-uy <58790294+blake-uy@users.noreply.github.com>
2021-06-24 16:27:41 -0700

    Create PoiController.java

---
+++ src/main/java/poi/controller/PoiController.java
@@ -0,0 +1,15 @@
+package poi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "poi")
+public class PoiController {
+
+    // TODO: Implement your API here.  You'll need to have Spring inject the
+    //      PoiManager object.  To do so, add a constructor that takes it as
+    //      a parameter.  Note that the BeanConfig class is already set up to
+    //      provide the object to the Spring framework as a Bean.
+
+}
